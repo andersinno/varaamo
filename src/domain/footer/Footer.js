243fbd4377410c30429c3744f139bea5a62cfa1d
Deepak Panta <deepak.panta@anders.fi>
2022-06-02 17:18:33 +0300

    Client registration description changes
    * Move the client registration description changes to footer
    * Change the finnish text from 'Asiakasrekisteriseloste' to
      'Tietosuojaseloste'
    * Update the link
    
    Refs TAM-134

--- src/domain/footer/Footer.js
+++ src/domain/footer/Footer.js
@@ -19,6 +19,11 @@ function Footer({ t }) {
       {t('AccessibilityPage.pageLink')}
     </Link>
   );
+  const clientRegisterDescriptionLink = (
+    <a href="https://www.tampere.fi/tietosuoja-ja-tiedonhallinta/tietosuojaselosteet">
+      {t('AboutPageContent.customerRegisterLink')}
+    </a>
+  );
 
   let cityNameId;
   switch (getCurrentCustomization()) {
@@ -51,6 +56,9 @@ function Footer({ t }) {
               {t(cityNameId)}
             </p>
             <MunicipalityLogos />
+            <p>
+              {clientRegisterDescriptionLink}
+            </p>
             <p>
               {accessibilityPage}
             </p>
