aa6444f27b078be93d2d3eac5349689cb3f2fecd
Deepak Panta <deepak.panta@anders.fi>
2022-07-25 15:12:30 +0300

    Use SHOW_TEST_SITE_MESSAGE as criteria for determining production link
    NODE_ENV was used to determine if the link to respaAdmin should
    point to production or test server. The value of QA's NODE_ENV is
    production, thus it was pointing to wrong RespaAdmin. Instead
    use the SHOW_TEST_SITE_MESSAGE enviroment variable.
    
    Refs None

--- src/domain/header/MainNavbarContainer.js
+++ src/domain/header/MainNavbarContainer.js
@@ -4,13 +4,14 @@ import { connect } from 'react-redux';
 import { clearSearchResults } from '../../../app/actions/searchActions';
 import { isAdminSelector, isLoggedInSelector } from '../../../app/state/selectors/authSelectors';
 import changeLocale from '../../../app/i18n/changeLocale';
+import constants from '../../../app/constants/AppConstants';
 import MainNavbar from './MainNavbar';
 
 export const selector = createStructuredSelector({
   isAdmin: isAdminSelector,
   isLoggedIn: isLoggedInSelector,
   respaAdminUrl: () => {
-    return process.env.NODE_ENV === 'production'
+    return constants.SHOW_TEST_SITE_MESSAGE
       ? 'https://respa.tampere.fi/ra/'
       : 'https://dev-respa.tampere.fi/ra/';
   },
