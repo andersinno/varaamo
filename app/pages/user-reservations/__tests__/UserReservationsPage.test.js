8fda5a9457bd86134e90cfefea7005b76ed210de
Pauliina Ilmanen <pauliina.jalava@anders.fi>
2025-03-18 14:45:48 +0200

    Fix UserReservationsPage test
    Refs TTVA-236, TTVA-241

--- app/pages/user-reservations/__tests__/UserReservationsPage.test.js
+++ app/pages/user-reservations/__tests__/UserReservationsPage.test.js
@@ -5,7 +5,20 @@ import { shallowWithIntl } from '../../../utils/testUtils';
 import { UnconnectedUserReservationsPage as UserReservationsPage } from '../UserReservationsPage';
 import ReservationList from '../reservation-list/ReservationListContainer';
 
-xdescribe('pages/user-reservations/UserReservationsPage', () => {
+// Mock the client module to avoid making actual API calls
+jest.mock('../../../../src/common/api/client', () => ({
+  __esModule: true,
+  default: {
+    get: jest.fn().mockResolvedValue({
+      data: {
+        results: [],
+        count: 0,
+      },
+    }),
+  },
+}));
+
+describe('pages/user-reservations/UserReservationsPage', () => {
   const defaultProps = {
     location: {
       search: '',
@@ -15,6 +28,8 @@ xdescribe('pages/user-reservations/UserReservationsPage', () => {
     },
     t: path => path,
     reduxReservations: {},
+
+    sendReservationsToRedux: jest.fn(),
   };
 
   function getWrapper(extraProps = {}) {
