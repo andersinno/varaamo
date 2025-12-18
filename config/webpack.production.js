00ab217c37916ad0079cfd096493c7d62f2df617
Jori Lindell <jori.lindell@gmail.com>
2025-12-18 13:47:06 +0200

    Refactor SCSS files to use `sass:color` and `sass:math` for color adjustments and calculations

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -34,7 +34,17 @@ module.exports = merge(common, {
           MiniCssExtractPlugin.loader,
           'css-loader',
           'resolve-url-loader',
-          { loader: 'sass-loader', options: { sassOptions: { sourceMap: true, sourceMapContents: false } } },
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                sourceMap: true,
+                sourceMapContents: false,
+                quietDeps: true,
+                silenceDeprecations: ['import', 'global-builtin', 'legacy-js-api'],
+              },
+            },
+          },
           { loader: 'postcss-loader', options: { plugins: [autoprefixer({ browsers: ['last 2 version', 'ie 9'] })] } },
         ],
       },
